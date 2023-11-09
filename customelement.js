59930b0b131c17cdcdf9410b5d5aafa3c76e75b3
Olga-steblyk <olga.steblyk@gmail.com>
2023-11-08 20:24:39 -0500

    help me again

--- customelement.js
+++ customelement.js
@@ -1,8 +1,14 @@
 class OlgaElement extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = "Help me";
+        this.innerHTML = "That's me in the corner";
     }
 }
 
 customElements.define("x-olga", OlgaElement);
 
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.htmlHTML =`<div style="text-align:center>${this.HTML}</div>`;
+    }
+}
+customElements.define("x-center", CenterElement);
