a9dc04e6e1b4cb4a4e4f17f7aabab144482bc3d6
Olga-steblyk <olga.steblyk@gmail.com>
2023-11-08 20:47:17 -0500

    slayyss

--- customelement.js
+++ customelement.js
@@ -8,7 +8,15 @@ customElements.define("x-olga", OlgaElement);
 
 class CenterElement extends HTMLElement{
     connectedCallback(){
-        this.htmlHTML =`<div style="text-align:center>${this.HTML}</div>`;
+        this.htmlHTML =`<div style="text-align:center>${this.innerHTML}</div>`;
     }
 }
 customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.htmlHTML = new Date().getFullYear();
+        
+    }
+}
+customElements.define("x-fullyear", YearElement);
