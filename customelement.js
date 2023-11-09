b1ebd97b134584d9719e41569711d1bc4811214c
Olga-steblyk <olga.steblyk@gmail.com>
2023-11-08 21:06:59 -0500

    thank you big C

--- customelement.js
+++ customelement.js
@@ -1,22 +1,22 @@
-class OlgaElement extends HTMLElement{
-    connectedCallback(){
+class OlgaElement extends HTMLElement {
+    connectedCallback() {
         this.innerHTML = "That's me in the corner";
     }
 }
 
 customElements.define("x-olga", OlgaElement);
 
-class CenterElement extends HTMLElement{
-    connectedCallback(){
-        this.htmlHTML =`<div style="text-align:center>${this.innerHTML}</div>`;
+class CenterElement extends HTMLElement {
+    connectedCallback() {
+        this.htmlHTML = `<div style="text-align:center">${this.innerHTML}</div>`;
     }
 }
 customElements.define("x-center", CenterElement);
 
-class YearElement extends HTMLElement{
-    connectedCallback(){
+class YearElement extends HTMLElement {
+    connectedCallback() {
         this.htmlHTML = new Date().getFullYear();
-        
+
     }
 }
 customElements.define("x-fullyear", YearElement);
